68930833d477316fd52fae4979eeba940cc73b66
Sajjad-arch <sajjadulislam870@gmail.com>
2024-12-14 22:57:34 +0600

    Add files via upload

---
+++ P12.C
@@ -0,0 +1,32 @@
+//Write a program in C to find whether a given number n is odd or even
+
+
+
+
+#include <stdio.h>
+
+int main (void)
+
+
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    if (n>0)
+        printf("n is the even number ", &n);
+
+    else if (n<0)
+        printf("n is the odd number ", &n);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
